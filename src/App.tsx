307036135485704a4a62b943034f0a32a679e827
alain.plana <alain.plana@free-now.com>
2022-02-22 07:38:33 +0100

    Project scafold

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />}>
+
+        </Route>
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
